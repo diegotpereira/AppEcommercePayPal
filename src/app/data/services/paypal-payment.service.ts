c7cc61ceac95bb15ad455fc90b6dd12ddd9f6ea6
Diego <diegoteixeirapereira@hotmail.com>
2021-09-06 17:37:40 -0300

    alteração nos arquivos TS service.

--- src/app/data/services/paypal-payment.service.ts
+++ src/app/data/services/paypal-payment.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
 import { HttpErrorHandlerService } from 'src/app/shared/services/http-error-handler.service';
 import { environment } from 'src/environments/environment';
+import { PaypalPayment } from '../models/paypal-payment';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +17,16 @@ export class PaypalPaymentService {
     private http: HttpClient,
     private eh: HttpErrorHandlerService
   ) { }
+
+  createPaypalPayment(payment: PaypalPayment) : Observable<PaypalPayment> {
+    return this.http.post<PaypalPayment>(this.url, payment).pipe(catchError(this.eh.handleError));
+  }
+
+  getPaypalPayment(id:string): Observable<PaypalPayment> {
+    return this.http.get<PaypalPayment>(`${this.url}/${id}`).pipe(catchError(this.eh.handleError));
+  }
+
+  updatePaypalment(id: string, paypalPayerId: string): Observable<PaypalPayment> {
+    return this.http.patch<PaypalPayment>(`${this.url}/${id}`, {paypalPayerId: paypalPayerId}).pipe(catchError(this.eh.handleError));
+  }
 }
