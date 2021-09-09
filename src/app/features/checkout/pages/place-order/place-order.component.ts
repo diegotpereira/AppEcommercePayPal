924bc783537a2439fc7cc16302deec1282441846
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-09-09 09:54:46 -0300

    alteração nos arquivos componentes.

--- src/app/features/checkout/pages/place-order/place-order.component.ts
+++ src/app/features/checkout/pages/place-order/place-order.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { iif } from 'rxjs';
+import { concatMap } from 'rxjs/operators';
+import { GetOrderParams, UpdateOrderParams } from 'src/app/data/models/order';
+import { CartService } from 'src/app/data/services/cart.service';
+import { OrderService } from 'src/app/data/services/order.service';
+import { PaypalPaymentService } from 'src/app/data/services/paypal-payment.service';
 
+
+@UntilDestroy({ checkProperties: true })
 @Component({
   selector: 'app-place-order',
   templateUrl: './place-order.component.html',
@@ -7,9 +18,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceOrderComponent implements OnInit {
 
-  constructor() { }
+  disableButton = true;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private payments: PaypalPaymentService,
+    private orders: OrderService,
+    private cart: CartService,
+    private snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit() {
+    this.route.queryParams
+    .pipe(
+      concatMap(params => {
+        const payerId = params['PayerID'];
+        const orderId = this.cart.orderId;
+
+        return iif(
+          () => payerId.length > 0,
+          this.orders.getOrder(orderId, GetOrderParams.paymentSource)
+          .pipe(
+            concatMap(order => {
+              const paymentSourceId = order.paymentSource?.id || '';
+              return iif(
+                () => paymentSourceId ? paymentSourceId.length > 0 : false,
+                this.payments.updatePaypalPayment(paymentSourceId, payerId)
+              );
+            })
+          )
+        );
+      })
+    )
+    .subscribe(
+      () => this.disableButton = false,
+      () => this.router.navigateByUrl('/error')
+    );
+  }
+
+  placeOrder() {
+    this.disableButton = true;
+
+    this.orders.updateOrder({
+      id: this.cart.orderId,
+      place: true
+    }, [UpdateOrderParams.place])
+    .subscribe(
+      () => {
+        this.snackBar.open('Seu pedido foi feito com sucesso.', 'Close', { duration: 3000});
+      },
+      () => {
+        this.snackBar.open('Ocorreu um problema ao fazer seu pedido.', 'Close', { duration: 8000});
+        this.disableButton = false;
+      }
+    );
   }
 
 }
